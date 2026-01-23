349aecbccb46801df1906b95c37ea2cf769840fd
Shadrack22788 <tuyizeresh@gmail.com>
2026-01-23 13:32:40 +0200

    saving portifolio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-green-600">
+        Saving Project 🚀
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
